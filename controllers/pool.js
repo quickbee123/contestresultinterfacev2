75b542e4d6a747e39d4f7064e3973ae5daa8e25d
quickbee123 <rohitrobin111@gmail.com>
2021-06-09 17:00:53 +0530

    deploy

--- controllers/pool.js
+++ controllers/pool.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   host: process.env.HOST_NAME,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: true
 });
 
 
